8938680aad93c2dbf7c3e7b925932017c1b222e5
Scott Horvath <shorvath208@gmail.com>
2017-12-25 21:44:56 -0600

    initial commit

---
+++ init.lua
@@ -0,0 +1,169 @@
+
+teruview = {}
+teruview.version = {1, 0, 0}
+local ver = teruview.version
+teruview.version_text = ver[1] .. '.' .. ver[2] .. '.' .. ver[3]
+teruview.mod_name = 'teruview'
+
+dofile(minetest.get_modpath(teruview.mod_name) .. '/options.lua')
+
+local private = {}
+private.hud_data = {}
+
+function teruview.get_hud_data(player)
+    return private.hud_data[player:get_player_name()]
+end
+
+function teruview.delete_hud_data(player)
+    private.hud_data[player:get_player_name()] = nil
+end
+
+function teruview.parse_mod_name(id)
+    return id:match('(%w+):')
+end
+
+function teruview.parse_node_id(id)
+    return id:match(':(.+)')
+end
+
+function teruview.init_hud_data(player)
+    local hud_data = {}
+    local player_name = player:get_player_name()
+    hud_data.bg = player:hud_add{
+        hud_elem_type = 'image',
+        position = teruview.anchor,
+        scale = {x=1.0, y=1.0},
+        offset = teruview.background_offset,
+        text = teruview.background_image,
+        alignment = 0
+    }
+    hud_data.name_text = player:hud_add{
+        hud_elem_type = 'text',
+        position = teruview.anchor,
+        offset = teruview.node_name_offset,
+        scale = {x=240, y=32},
+        text = '<left-click for information>',
+        alignment = 0,
+        number = teruview.view_color_nothing
+    }
+    hud_data.mod_text = player:hud_add{
+        hud_elem_type = 'text',
+        position = teruview.anchor,
+        offset = teruview.mod_name_offset,
+        scale = {x=240, y=32},
+        text = 'Teruview version ' .. teruview.version_text,
+        alignment = 0,
+        number = teruview.view_color_modname
+    }
+    hud_data.tool_info_text = player:hud_add{
+        hud_elem_type = 'text',
+        position = teruview.anchor,
+        offset = teruview.tool_info_offset,
+        scale = {x=80, y=32},
+        text = '',
+        alignment = 0,
+        number = teruview.view_color_nothing
+    }
+    hud_data.node_info_text = player:hud_add{
+        hud_elem_type = 'text',
+        position = teruview.anchor,
+        offset = teruview.node_info_offset,
+        scale = {x=80, y=32},
+        text = '',
+        alignment = 0,
+        number = teruview.view_color_nothing
+    }
+    hud_data.display_time = teruview.view_display_time
+    private.hud_data[player_name] = hud_data
+end
+
+function private.section_update(player, hud_elem, text, text_color)
+    player:hud_change(hud_elem, 'text', text)
+    player:hud_change(hud_elem, 'number', text_color)
+end
+
+function teruview.update_view(pos, node, player, pointed_thing)
+    local hud_data = teruview.get_hud_data(player)
+    local update = {}
+    update.name = '<nothing>'
+    update.mod = '<none>'
+    update.name_color = teruview.view_color_nothing
+    update.mod_color = teruview.view_color_undefined
+    update.tools = '<no tools>'
+    update.tools_color = teruview.view_tool_none
+    update.flags = ''
+    update.flags_color = teruview.view_color_nothing
+    if node then
+        local node_data = minetest.registered_nodes[node.name]
+        if node_data then
+            update.mod = teruview.parse_mod_name(node.name)
+            update.mod_color = teruview.view_color_modname
+            -- read node name (prefer description, ID as last resort)
+            update.name = node_data.description
+            if update.name and #update.name > 0 then
+                update.name_color = teruview.view_color_node_name
+            else
+                update.name = teruview.parse_node_id(node.name)
+                update.name_color = teruview.view_color_node_id
+            end
+            -- read and parse node groups (for listing tools and flags)
+            local wielded_data = minetest.registered_items[player:get_wielded_item():get_name()]
+            update.tools = ''
+            update.tools_color = teruview.view_tool_mismatch
+            for grp, level in pairs(node_data.groups) do
+                if teruview.info_node_groups[grp] then
+                    update.flags = teruview.info_node_groups[grp] .. ' ' .. update.flags
+                    update.flags_color = teruview.view_node_info
+                end
+                if teruview.tool_node_groups[grp] then
+                    if wielded_data and wielded_data.tool_capabilities and wielded_data.tool_capabilities.groupcaps and wielded_data.tool_capabilities.groupcaps[grp] then
+                        update.tools_color = teruview.view_tool_match
+                    end
+                    update.tools = teruview.tool_node_groups[grp] .. ':' .. (teruview.tool_group_level_description[node_data.groups[grp]] or 'Unk.') .. ' ' .. update.tools
+                end
+            end
+        else
+            -- case that there is no registered data for node
+            update.name = node.name
+            update.mod = '<undefined>'
+            update.name_color = teruview.view_color_undefined
+            update.mod_color = teruview.view_color_undefined
+        end
+    end
+    -- update actual HUD objects with new information
+    private.section_update(player, hud_data.name_text, update.name, update.name_color)
+    private.section_update(player, hud_data.mod_text, update.mod, update.mod_color)
+    private.section_update(player, hud_data.tool_info_text, update.tools, update.tools_color)
+    private.section_update(player, hud_data.node_info_text, update.flags, update.flags_color)
+    player:hud_change(hud_data.bg, 'text', teruview.background_image)
+
+    -- set disappearance timer
+    hud_data.display_time = teruview.view_display_time
+end
+
+function teruview.time_hud(player, dt)
+    local hud_data = teruview.get_hud_data(player)
+    if hud_data.display_time and hud_data.display_time > 0.0 then
+        hud_data.display_time = hud_data.display_time - dt
+        if hud_data.display_time <= 0.0 then
+            player:hud_change(hud_data.name_text, 'text', '')
+            player:hud_change(hud_data.mod_text, 'text', '')
+            player:hud_change(hud_data.bg, 'text', '')
+            player:hud_change(hud_data.tool_info_text, 'text', '')
+            player:hud_change(hud_data.node_info_text, 'text', '')
+            hud_data.display_time = nil
+        end
+    end
+end
+
+minetest.register_on_joinplayer(teruview.init_hud_data)
+
+minetest.register_on_leaveplayer(teruview.delete_hud_data)
+
+minetest.register_on_punchnode(teruview.update_view)
+
+minetest.register_globalstep(function(dt)
+    for _,pl in ipairs(minetest.get_connected_players()) do
+        teruview.time_hud(pl, dt)
+    end
+end)
---
+++ options.lua
@@ -0,0 +1,67 @@
+-- Area on screen that information box appears as a percentage of the whole screen.
+-- {x=0.5, y=0.0} is top-center of screen
+-- {x=0.1, y=0.1} is top-left of screen, etc.
+-- (see description of "position" HUD attribute in minetest's lua_api.txt for more information)
+teruview.anchor = {x=0.5, y=0.0}
+
+-- Background of information box
+teruview.background_image = 'teruview_simple_bg.png'
+-- X/Y offset of information box image
+teruview.background_offset = {x=0, y=48}
+
+-- X/Y offsets for individual text sections
+teruview.node_name_offset = {x=0, y=24}
+teruview.mod_name_offset = {x=0, y=40}
+teruview.tool_info_offset = {x=0, y=56}
+teruview.node_info_offset = {x=0, y=72}
+
+-- Text for displaying node group types. Also, existence in this table signifies a tool group should be displayed at all.
+-- By default shows by preferred tool type rather than the more abstract names minetest uses internally.
+teruview.tool_node_groups = {
+    cracky='Pickaxe',
+    crumbly='Shovel',
+    choppy='Axe',
+    snappy='Sword',
+    oddly_breakable_by_hand='Hand'
+}
+
+-- Text for displaying other node features.
+teruview.info_node_groups = {
+    flammable='Flammable',
+    puts_out_fire='Anti-fire',
+    cools_lava='Anti-lava',
+    leafdecay='Decays',
+    fleshy='Organic',
+    dig_immediate='Instant',
+    falling_node='Gravity'
+}
+
+-- Time information box remains on-screen after initial punch (in seconds)
+teruview.view_display_time = 3.5
+
+-- All colors are in the form of hexadecimal 0xRRGGBB
+-- Text color for purely informational messages
+teruview.view_color_nothing = 0x606060
+-- Text color for undefined nodes
+teruview.view_color_undefined = 0xFF0000
+-- Text color for description of a node
+teruview.view_color_node_name = 0xFFFFFF
+-- Text color for base ID of a node without description
+teruview.view_color_node_id = 0xFFFF00
+-- Text color for mod name
+teruview.view_color_modname = 0x6ea8ff
+
+-- Text color for when current tool level matches type
+teruview.view_tool_match = 0x8bffbc
+-- Text color for when current tool does not match node type
+teruview.view_tool_mismatch = 0xFFC78B
+-- Text color for when node has no tools defined
+teruview.view_tool_none = 0xA0A0A0
+
+-- Text color for node flags
+teruview.view_node_info = 0xC0C0C0
+
+-- Text for describing node group "levels". In vanilla minetest, group 1 is slowest and group 3 is fastest to dig/mine.
+teruview.tool_group_level_description = {
+    'Slow', 'Avg.', 'Fast'
+}
